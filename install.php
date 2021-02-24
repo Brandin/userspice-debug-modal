dd37e920622cd02ecfe33dc4c16a7265b5e74de7
Brandin Arsenault <15201621+brandinarsenault@users.noreply.github.com>
2021-02-24 18:59:20 -0400

    Remove unused hook

--- install.php
+++ install.php
@@ -1,31 +1,28 @@
 <?php
-require_once("init.php");
-if (in_array($user->data()->id, $master_account)){
 
+require_once 'init.php';
+if (in_array($user->data()->id, $master_account)) {
+    $db = DB::getInstance();
+    include 'plugin_info.php';
 
-	$db = DB::getInstance();
-	include "plugin_info.php";
+    $check = $db->query('SELECT * FROM us_plugins WHERE plugin = ?', [$plugin_name])->count();
+    if ($check > 0) {
+        err($plugin_name.' has already been installed!');
+    } else {
+        $fields = [
+            'plugin' => $plugin_name,
+            'status' => 'installed',
+        ];
+        $db->insert('us_plugins', $fields);
+        if (!$db->error()) {
+            err($plugin_name.' installed');
+            logger($user->data()->id, 'USPlugins', $plugin_name.' installed');
+        } else {
+            err($plugin_name.' was not installed');
+            logger($user->data()->id, 'USPlugins', 'Failed to to install plugin, Error: '.$db->errorString());
+        }
+    }
 
-	$check = $db->query("SELECT * FROM us_plugins WHERE plugin = ?",array($plugin_name))->count();
-	if($check > 0){
-		err($plugin_name.' has already been installed!');
-	}else{
-		$fields = array(
-			'plugin'=>$plugin_name,
-			'status'=>'installed',
-		);
-		$db->insert('us_plugins',$fields);
-		if(!$db->error()) {
-			err($plugin_name.' installed');
-			logger($user->data()->id,"USPlugins",$plugin_name." installed");
-		} else {
-			err($plugin_name.' was not installed');
-			logger($user->data()->id,"USPlugins","Failed to to install plugin, Error: ".$db->errorString());
-		}
-	}
-
-	$hooks = [];
-	$hooks['login.php']['bottom'] = 'hooks/loginbottom.php';
-	registerHooks($hooks,$plugin_name);
-
+    $hooks = [];
+    registerHooks($hooks, $plugin_name);
 }
